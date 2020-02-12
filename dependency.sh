0f2835d3dd8e61fb25affb15dd6bfa6909bbbf34
Khurush Bengali <56169070+khurushbengali@users.noreply.github.com>
2020-02-12 12:21:28 +0530

    Add files via upload

---
+++ dependency.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "SAVING FIRST FILE"
+mkdir /home/khurush/Downloads/First
+sudo cat > /home/khurush/Downloads/First/flag1.txt << "EOF"
+CDAKJSHFNKJEL234GFLL9231BWFL1
+EOF
+
+echo "SAVING SECOND FILE:" 
+mkdir /home/khurush/Music/Second
+sudo cat > /home/khurush/Music/Second/flag2.txt << "EOF"
+OIJONKJBHBHJBUGBSDWE123FL2
+EOF
+
+echo "SAVING THIRD FILE"
+sudo mkdir /media/Third
+sudo cat > /media/Third/flag3.txt << "EOF"
+HGRV2FVUYV213Y4UBUAHJOIEJDFL3
+EOF
+
+echo "SAVING FOURTH FILE"
+sudo mkdir /root/Fourth 
+sudo cat > /root/Fourth/flag4.txt << "EOF"
+BLIV3EREJH3B554CU7HWBMLPOFL4
+EOF
+
+echo "SAVING FIFTH FILE"
+sudo mkdir /lib32/Fifth
+sudo cat > /lib32/Fifth/flag5.txt << "EOF"
+JDALSHFUIAHILEFBIASBCVAYUSBD
+EOF
+
+echo "SAVING SIXTH FILE" 
+sudo mkdir /mnt/Sixth
+sudo cat > /mnt/Sixth/flag6.txt << "EOF"
+AWECBABVBAYWEBRUVBAUSBDHF23
+EOF
+
+echo "SAVING SEVENTH FILE"
+sudo mkdir /tmp/Seventh
+sudo cat > /tmp/Seventh/flag7.txt << "EOF"
+NBNASD23BFUABUBAW5UBEBFUAWB
+EOF
+
+echo "RUNNING DEPENDENCY SCRIPT"
+
+python3 hint2_dependency.py
+
+echo "DONE"
